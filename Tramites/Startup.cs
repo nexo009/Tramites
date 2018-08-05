c9ab99f995a477dd7fed39fcc4c01309fd035594
Nelson Neftali Hernandez Sermenio <nhernandez@DGSP.CORP>
2018-08-05 09:37:08 -0600

    Add project files.

---
+++ Tramites/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Tramites.Startup))]
+namespace Tramites
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
